2294a256a2e6968df5c106a04721998e5c3d181e
shareef <nadeemshareef934@gmail.com>
2022-04-05 10:53:52 +0530

    Remove unused files

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Engineering Overflow</title>
+      </Head>
+      <div>
+        <h1>Hello World!</h1>
+      </div>
+    </>
+  );
+}
