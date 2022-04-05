c3e0c4da00f3bd626b2046862a9f02838f1d64a3
shareef <nadeemshareef934@gmail.com>
2022-04-05 11:17:42 +0530

    design header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Link from "next/Link";
 
 export default function Home() {
   return (
@@ -6,9 +7,25 @@ export default function Home() {
       <Head>
         <title>Engineering Overflow</title>
       </Head>
-      <div>
-        <h1 className="text-red-100">Hello World!</h1>
-      </div>
+      <>
+        <header className="container flex items-center justify-between my-4 border-b-2 border-primary-text pb-4">
+          <div id="logo">
+            <span className="font-bold text-2xl">
+              <Link href="/">Engineering Overflow</Link>
+            </span>
+          </div>
+          <nav>
+            <ul>
+              <li>
+                {" "}
+                <Link href="/login">Login</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <main></main>
+        <footer></footer>
+      </>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,27 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: "2rem",
+    },
+    screens: {
+      xs: "475px",
+      ...defaultTheme.screens,
+    },
+    extend: {
+      colors: {
+        primary: {
+          bg: "var(--primary-bg)",
+          text: "var(--primary-text)",
+        },
+      },
+    },
   },
   plugins: [],
 };
