5c06141622d2d35ca87ace0d37cdcd176ecd89d2
shareef <nadeemshareef934@gmail.com>
2022-04-05 11:43:30 +0530

    design landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,8 +23,48 @@ export default function Home() {
             </ul>
           </nav>
         </header>
-        <main></main>
-        <footer></footer>
+        <main className="px-[15%]">
+          <section>
+            <div className="flex justify-between my-8">
+              <h2 className="text-xl">Top Questions</h2>
+              <Link href="/askQuestion" as="ask-question">
+                <button>Ask Question</button>
+              </Link>
+            </div>
+            <div className="flex px-[5%]">
+              <div className="flex flex-col items-end">
+                <div className="flex space-x-2">
+                  <span>0</span>
+                  <span>votes</span>
+                </div>
+                <div className="flex space-x-2">
+                  <span>11</span>
+                  <span>answers</span>
+                </div>
+                <div className="flex space-x-2">
+                  <span>99</span>
+                  <span>views</span>
+                </div>
+              </div>
+              <div className="pl-6">
+                <p className="font-semibold text-xl">
+                  How to score good marks in mathematics III
+                </p>
+                <div className="pt-2">
+                  <span className="bg-primary-text text-primary-bg p-1 mx-1 rounded opacity-85">
+                    Maths
+                  </span>
+                  <span className="bg-primary-text text-primary-bg p-1 mx-1 rounded opacity-85">
+                    Marks
+                  </span>
+                  <span className="bg-primary-text text-primary-bg p-1 mx-1 rounded opacity-85">
+                    Tag 3
+                  </span>
+                </div>
+              </div>
+            </div>
+          </section>
+        </main>
       </>
     </>
   );
